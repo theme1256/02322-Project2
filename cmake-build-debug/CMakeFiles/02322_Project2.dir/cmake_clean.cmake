c8b5d7b463ac04eabe907e482bfc9e04982aa30b
Folkmann <theme1256@gmail.com>
2020-05-11 16:11:34 +0200

    Tomt projekt fra CLion

---
+++ cmake-build-debug/CMakeFiles/02322_Project2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "02322_Project2.exe"
+  "02322_Project2.exe.manifest"
+  "02322_Project2.lib"
+  "02322_Project2.pdb"
+  "CMakeFiles/02322_Project2.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/02322_Project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
