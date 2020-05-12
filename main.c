e70110a9dd5f37eaa8ac6c7bc5d0d163df58686c
Folkmann <theme1256@gmail.com>
2020-05-12 16:39:18 +0200

    Burde kunne læse en .asm fil nu og konvertere til .bin, ikke testet

--- main.c
+++ main.c
@@ -15,8 +15,10 @@ void decStringToBinary(char *rtn, char *ptr, int length, int offset);
 
 int main() {
     // Init variables
+    char in_file[32];
+    char out_file[32];
+    FILE *f_i, *f_o;
     char input[MAX];
-    char original[MAX];
     char output[MAX];
     char empty[] = "";
     char delim[] = " ,\n";
@@ -24,226 +26,229 @@ int main() {
 
     // Init infinite loop
     while (1) {
-        strcpy(output, empty);
-        // Get input from stdin and make a copy for later use
-        printf("Input an LC3 assembly instruction:");
-        fgets(input, MAX, stdin);
-        strcpy(original, input);
+        // Get filename from user
+        printf("Input filename (without .asm):");
+        fgets(in_file, MAX, stdin);
 
-        // Split input by space and comma and remove trailing \n
-        char *ptr = strtok(input, delim);
+        // Compose filenames for in put out files
+        strcpy(out_file, in_file);
+        strcat(in_file, ".asm");
+        strcat(out_file, ".bin");
 
-        /* Try to find which of the known commands */
-        if (strcmp(ptr, "ADD") == 0) {
-            printf("Found ADD, converting to 0001\n");
-            strcat(output, "0001");
-            // Move pointer to next split, should be DR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next split, should be SR1
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next split, should be SR2 or imm5
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is number or register next
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                // Not a register, must be imm5, add a 1 to output
-                strcat(output, "1");
-                // Do a conversion from string with decimal number to binary string
-                char bin[5];
-                decStringToBinary(bin, ptr, 5, 1);
-                printf("Converting: %s to binary: %s\n", ptr, bin);
-                strcat(output, bin);
-            } else {
-                // It was a register, add to output
-                strcat(output, "000");
+        // Open files in read and write mode
+        f_i = fopen(in_file, "r");
+        f_o = fopen(out_file, "w");
+
+        // Loop through entire input-file
+        while (fgets(input, MAX, f_i)) {
+            // Clear output
+            strcpy(output, empty);
+
+            // Split input by space and comma and remove trailing \n
+            char *ptr = strtok(input, delim);
+
+            /* Try to find which of the known commands */
+            if (strcmp(ptr, "ADD") == 0) {
+                printf("Found ADD, converting to 0001\n");
+                strcat(output, "0001");
+                // Move pointer to next split, should be DR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
                 strcat(output, test);
+                // Move pointer to next split, should be SR1
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next split, should be SR2 or imm5
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is number or register next
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    // Not a register, must be imm5, add a 1 to output
+                    strcat(output, "1");
+                    // Do a conversion from string with decimal number to binary string
+                    char bin[5];
+                    decStringToBinary(bin, ptr, 5, 1);
+                    printf("Converting: %s to binary: %s\n", ptr, bin);
+                    strcat(output, bin);
+                } else {
+                    // It was a register, add to output
+                    strcat(output, "000");
+                    strcat(output, test);
+                }
             }
-        }
-        else if (strcmp(ptr, "NOT") == 0) {
-            printf("Found NOT, converting to 1001\n");
-            strcat(output, "1001");
-            // Move pointer to next split, should be DR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
+            else if (strcmp(ptr, "NOT") == 0) {
+                printf("Found NOT, converting to 1001\n");
+                strcat(output, "1001");
+                // Move pointer to next split, should be DR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next split, should be SR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Add 6 ones
+                strcat(output, "111111");
             }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next split, should be SR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
+            else if (strstr(ptr, "BR") != NULL) {
+                printf("Found BR, converting to 0000\n");
+                strcat(output, "0000");
+                // check for flags n, z and/or p and add a 1 or 0
+                if (strchr(ptr, 'n') != NULL) {
+                    strcat(output, "1");
+                } else {
+                    strcat(output, "0");
+                }
+                if (strchr(ptr, 'z') != NULL) {
+                    strcat(output, "1");
+                } else {
+                    strcat(output, "0");
+                }
+                if (strchr(ptr, 'p') != NULL) {
+                    strcat(output, "1");
+                } else {
+                    strcat(output, "0");
+                }
+                // Move pointer to next split, should be a label or an offset
+                ptr = strtok(NULL, delim);
+                if (strchr(ptr, '#') != NULL) {
+                    // It's an offset, convert to binary
+                    char bin[9];
+                    decStringToBinary(bin, ptr, 9, 1);
+                    printf("Converting: %s to binary: %s\n", ptr, bin);
+                    strcat(output, bin);
+                } else {
+                    // It's a label
+                    printf("Labels are not supported\n");
+                    continue;
+                }
             }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Add 6 ones
-            strcat(output, "111111");
-        }
-        else if (strstr(ptr, "BR") != NULL) {
-            printf("Found BR, converting to 0000\n");
-            strcat(output, "0000");
-            // check for flags n, z and/or p and add a 1 or 0
-            if (strchr(ptr, 'n') != NULL) {
-                strcat(output, "1");
-            } else {
-                strcat(output, "0");
+            else if (strcmp(ptr, "LD") == 0) {
+                printf("Found LD, converting to 0010\n");
+                strcat(output, "0010");
+                // Move pointer to next split, should be DR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next word, should be an offset
+                ptr = strtok(NULL, delim);
+                if (strchr(ptr, '#') != NULL) {
+                    // It's a value, convert to binary and add to output
+                    char bin[9];
+                    decStringToBinary(bin, ptr, 9, 1);
+                    printf("Converting: %s to binary: %s\n", ptr, bin);
+                    strcat(output, bin);
+                } else {
+                    // It's a label
+                    printf("Labels are not supported\n");
+                    continue;
+                }
             }
-            if (strchr(ptr, 'z') != NULL) {
-                strcat(output, "1");
-            } else {
-                strcat(output, "0");
+            else if (strcmp(ptr, "LDR") == 0) {
+                printf("Found LDR, converting to 0110\n");
+                strcat(output, "0110");
+                // Move pointer to next split, should be DR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next word, should be a register
+                ptr = strtok(NULL, delim);
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next word, should be an offset
+                ptr = strtok(NULL, delim);
+                if (strchr(ptr, '#') != NULL) {
+                    // It's a value, convert to binary and add to output
+                    char bin[9];
+                    decStringToBinary(bin, ptr, 9, 1);
+                    printf("Converting: %s to binary: %s\n", ptr, bin);
+                    strcat(output, bin);
+                } else {
+                    // It's a label
+                    printf("Labels are not supported\n");
+                    continue;
+                }
             }
-            if (strchr(ptr, 'p') != NULL) {
-                strcat(output, "1");
-            } else {
-                strcat(output, "0");
-            }
-            // Move pointer to next split, should be a label or an offset
-            ptr = strtok(NULL, delim);
-            if (strchr(ptr, '#') != NULL) {
-                // It's an offset, convert to binary
-                char bin[9];
-                decStringToBinary(bin, ptr, 9, 1);
-                printf("Converting: %s to binary: %s\n", ptr, bin);
-                strcat(output, bin);
+            else if (strcmp(ptr, "ST") == 0) {
+                printf("Found ST, converting to 0011\n");
+                strcat(output, "0011");
+                // Move pointer to next split, should be SR
+                ptr = strtok(NULL, delim);
+                // Try to find out if it is R1-R7
+                findRegister(test, ptr);
+                if (strcmp(test, "") == 0) {
+                    printf("Didn't find a register, try again\n");
+                    continue;
+                }
+                // Add the binary equivalent to output
+                strcat(output, test);
+                // Move pointer to next word, should be an offset
+                ptr = strtok(NULL, delim);
+                if (strchr(ptr, '#') != NULL) {
+                    // It's a value, convert to binary and add to output
+                    char bin[9];
+                    decStringToBinary(bin, ptr, 9, 1);
+                    printf("Converting: %s to binary: %s\n", ptr, bin);
+                    strcat(output, bin);
+                } else {
+                    // It's a label
+                    printf("Labels are not supported\n");
+                    continue;
+                }
             }
             else {
-                // It's a label
-                printf("Labels are not supported\n");
+                printf("Didn't recognize instruction, try again\n");
                 continue;
             }
-        }
-        else if (strcmp(ptr, "LD") == 0) {
-            printf("Found LD, converting to 0010\n");
-            strcat(output, "0010");
-            // Move pointer to next split, should be DR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next word, should be an offset
-            ptr = strtok(NULL, delim);
-            if (strchr(ptr, '#') != NULL) {
-                // It's a value, convert to binary and add to output
-                char bin[9];
-                decStringToBinary(bin, ptr, 9, 1);
-                printf("Converting: %s to binary: %s\n", ptr, bin);
-                strcat(output, bin);
-            }
-            else {
-                // It's a label
-                printf("Labels are not supported\n");
-                continue;
-            }
-        }
-        else if (strcmp(ptr, "LDR") == 0) {
-            printf("Found LDR, converting to 0110\n");
-            strcat(output, "0110");
-            // Move pointer to next split, should be DR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next word, should be a register
-            ptr = strtok(NULL, delim);
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next word, should be an offset
-            ptr = strtok(NULL, delim);
-            if (strchr(ptr, '#') != NULL) {
-                // It's a value, convert to binary and add to output
-                char bin[9];
-                decStringToBinary(bin, ptr, 9, 1);
-                printf("Converting: %s to binary: %s\n", ptr, bin);
-                strcat(output, bin);
-            }
-            else {
-                // It's a label
-                printf("Labels are not supported\n");
-                continue;
-            }
-        }
-        else if (strcmp(ptr, "ST") == 0) {
-            printf("Found ST, converting to 0011\n");
-            strcat(output, "0011");
-            // Move pointer to next split, should be SR
-            ptr = strtok(NULL, delim);
-            // Try to find out if it is R1-R7
-            findRegister(test, ptr);
-            if (strcmp(test, "") == 0) {
-                printf("Didn't find a register, try again\n");
-                continue;
-            }
-            // Add the binary equivalent to output
-            strcat(output, test);
-            // Move pointer to next word, should be an offset
-            ptr = strtok(NULL, delim);
-            if (strchr(ptr, '#') != NULL) {
-                // It's a value, convert to binary and add to output
-                char bin[9];
-                decStringToBinary(bin, ptr, 9, 1);
-                printf("Converting: %s to binary: %s\n", ptr, bin);
-                strcat(output, bin);
-            }
-            else {
-                // It's a label
-                printf("Labels are not supported\n");
-                continue;
-            }
-        }
-        else {
-            printf("Didn't recognize instruction, try again\n");
-            continue;
-        }
-        /**/
+            /**/
 
-        /* This loop is used to debug the string-splitting
-        while (ptr != NULL) {
-            printf("'%s'\n", ptr);
-            ptr = strtok(NULL, delim);
+            // Write binary to file
+            fputc(output, f_o);
         }
-        /**/
-
-        // Print the original command and the binary equivalent
-        printf("\nYou entered: %s", original);
-        printf("Binary equivalent: %s\n", output);
+        fclose(f_i);
+        fclose(f_o);
     }
     return 0;
 }
